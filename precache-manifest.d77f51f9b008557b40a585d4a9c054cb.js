711d70363c02c8c7f9ac94f642e5aeacdc6715e8
clairehq <claireli.hq@gmail.com>
2020-05-03 20:44:37 -0700

    Updates

---
+++ precache-manifest.d77f51f9b008557b40a585d4a9c054cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30db45e5bc4ecf3bb6af6539d8654ed",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0ef0b578cd77cee5c685",
+    "url": "/pokedex/static/css/main.c43d3e1b.chunk.css"
+  },
+  {
+    "revision": "44dc7576a69afcca446b",
+    "url": "/pokedex/static/js/2.38078485.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.38078485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef0b578cd77cee5c685",
+    "url": "/pokedex/static/js/main.228f5fb4.chunk.js"
+  },
+  {
+    "revision": "d367ed54df4579fc06b7",
+    "url": "/pokedex/static/js/runtime-main.a8c1be9c.js"
+  }
+]);
